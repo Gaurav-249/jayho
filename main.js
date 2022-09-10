3364f462b51d1ac03d7add147b1bac83b53f9df0
mnamegauravsingh <neelusingh249sips@gmail.com>
2022-09-10 22:01:36 +0530

    last commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function g()
+{
+	document.getElementById('p2').style.color='green';
+}
+function o()
+{
+	document.getElementById('p2').style.color='yellow';
+}
+function s()
+{
+	document.getElementById('p2').style.color='maroon';
+}
+function m()
+{
+	document.getElementById('p2').style.color='aqua';
+}
